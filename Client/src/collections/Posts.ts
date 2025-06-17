1b918e2dd4c249400cf35f28e560d9df38bd0885
John Patrick Trinidad <Ptrinidad765@gmail.com>
2025-06-17 13:43:28 +0800

    added Posts collection

---
+++ Client/src/collections/Posts.ts
@@ -0,0 +1,19 @@
+import type { CollectionConfig } from 'payload'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+      editor: lexicalEditor({})
+    },
+  ],
+}
